bfb42427b73c5045e41da11920f2476efff32bbe
Sabin Maharjan <sabin007.maharjan@gmail.com>
2021-08-25 19:30:29 +0545

    Day3

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-var http = require('http');
-var url = require('url');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8000
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
-}).listen(8080);
+app.get('/',(req,res) =>{
+  res.sendFile(path.join(__dirname,"index.html"))
+})
+app.get('/hello/:name',(req,res) =>{
+  res.send("Hello " + req.params.name)
+})
+app.get('/about',(req,res) =>{
+  res.sendFile(path.join(__dirname,"about.html"))
+})
+app.get('/error',(req,res) =>{
+  res.sendFile(path.join(__dirname,"error.html"))
+})
+
+
+
+app.listen(port, ()=>{
+  console.log(`Node js running at http://localhost:${port}/`)
+}) 
